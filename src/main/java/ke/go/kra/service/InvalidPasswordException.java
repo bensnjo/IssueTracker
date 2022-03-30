783241bb4c2e4232aeea257b6f8f111dc09f9b3b
benson <bensnjo@gmail.com>
2022-03-30 09:49:12 +0300

    Initial version of IssueTracker generated by generator-jhipster@7.7.0

---
+++ src/main/java/ke/go/kra/service/InvalidPasswordException.java
@@ -0,0 +1,10 @@
+package ke.go.kra.service;
+
+public class InvalidPasswordException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public InvalidPasswordException() {
+        super("Incorrect password");
+    }
+}
